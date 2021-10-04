52ff8ae34fe361ef7a8498e2fe61015cba66565d
JonasGarciaGit <jonasemprego1@gmail.com>
2021-10-04 00:17:43 -0300

    Switching background

--- Assets/Scripts/IncreaseDifficulty.cs
+++ Assets/Scripts/IncreaseDifficulty.cs
@@ -9,11 +9,14 @@ public class IncreaseDifficulty : MonoBehaviour
     public GameObject starDropping;
     public GameObject starBackground;
     public GameObject fuelDroppingSpeed;
+    public SpriteRenderer backgroundRenderer;
+    public List<Sprite> bkImages;
 
     private void Start()
     {
         distance = 0;
         StartCoroutine("increaseDistance");
+        StartCoroutine("backgroundTransition");
     }
 
     // Update is called once per frame
@@ -23,69 +26,34 @@ public class IncreaseDifficulty : MonoBehaviour
 
         if (distance == 1)
         {
-            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = 0.68f;
-            starDropping.GetComponent<ParticleSystem>().playbackSpeed = 1.2f;
-            starBackground.GetComponent<ParticleSystem>().playbackSpeed = 2.2f;
-            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = 4.9f;
-        }
-        else if (distance == 2)
-        {
-            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = 0.66f;
-            starDropping.GetComponent<ParticleSystem>().playbackSpeed = 1.4f;
-            starBackground.GetComponent<ParticleSystem>().playbackSpeed = 2.4f;
-            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = 4.8f;
+            backgroundRenderer.gameObject.SetActive(true);
+            backgroundRenderer.sprite = bkImages[0];
         }
         else if (distance == 3)
         {
-            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = 0.64f;
-            starDropping.GetComponent<ParticleSystem>().playbackSpeed = 1.6f;
-            starBackground.GetComponent<ParticleSystem>().playbackSpeed = 2.6f;
-            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = 4.7f;
+            backgroundRenderer.flipX = true;
+            backgroundRenderer.sprite = bkImages[1];
         }
         else if (distance == 4)
         {
-            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = 0.62f;
-            starDropping.GetComponent<ParticleSystem>().playbackSpeed = 1.8f;
-            starBackground.GetComponent<ParticleSystem>().playbackSpeed = 2.8f;
-            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = 4.6f;
-        }
-        else if (distance == 5)
-        {
-            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = 0.6f;
-            starDropping.GetComponent<ParticleSystem>().playbackSpeed = 2f;
-            starBackground.GetComponent<ParticleSystem>().playbackSpeed = 3f;
-            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = 4.5f;
+            backgroundRenderer.flipX = false;
+            backgroundRenderer.gameObject.transform.position = new Vector3(16f, 9.8f, 45f);
+            backgroundRenderer.sprite = bkImages[2];
         }
         else if (distance == 6)
         {
-            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = 0.58f;
-            starDropping.GetComponent<ParticleSystem>().playbackSpeed = 2.2f;
-            starBackground.GetComponent<ParticleSystem>().playbackSpeed = 3.2f;
-            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = 4.4f;
-        }
-        else if (distance == 7)
-        {
-            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = 0.56f;
-            starDropping.GetComponent<ParticleSystem>().playbackSpeed = 2.4f;
-            starBackground.GetComponent<ParticleSystem>().playbackSpeed = 3.4f;
-            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = 4.3f;
-        
+            backgroundRenderer.gameObject.transform.position = new Vector3(-9.8f, 9.8f, 45f);
+            backgroundRenderer.sprite = bkImages[3];
         }
         else if (distance == 8)
         {
-            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = 0.54f;
-            starDropping.GetComponent<ParticleSystem>().playbackSpeed = 2.6f;
-            starBackground.GetComponent<ParticleSystem>().playbackSpeed = 3.6f;
-            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = 4.2f;
-          
+            backgroundRenderer.gameObject.transform.position = new Vector3(-9.8f, 9.8f, 45f);
+            backgroundRenderer.sprite = bkImages[4];
         }
         else if (distance == 9)
         {
-            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = 0.52f;
-            starDropping.GetComponent<ParticleSystem>().playbackSpeed = 2.8f;
-            starBackground.GetComponent<ParticleSystem>().playbackSpeed = 3.8f;
-            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = 4.1f;
-
+            backgroundRenderer.gameObject.transform.position = new Vector3(-3.3f, 9.8f, 45f);
+            backgroundRenderer.sprite = bkImages[5];
         }
     }
 
@@ -93,12 +61,77 @@ public class IncreaseDifficulty : MonoBehaviour
     {
         while(distance != 9)
         {
-            yield return new WaitForSeconds(120f);
+            yield return new WaitForSeconds(30f);
             this.distance = this.distance + 1;
-            Debug.Log("asteroid speed .: " + asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime);
-            Debug.Log("starDropping speed .: " + starDropping.GetComponent<ParticleSystem>().playbackSpeed);
-            Debug.Log("starBackground speed .: " + starBackground.GetComponent<ParticleSystem>().playbackSpeed);
-            Debug.Log("fuel speed .: " + fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed);
+
+            asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime = asteroidRespawn.GetComponent<AsteroidSpawner>().respawnTime - 0.02f;
+            starDropping.GetComponent<ParticleSystem>().playbackSpeed = starDropping.GetComponent<ParticleSystem>().playbackSpeed + 0.2f;
+            starBackground.GetComponent<ParticleSystem>().playbackSpeed = starBackground.GetComponent<ParticleSystem>().playbackSpeed + 0.2f;
+            fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed = fuelDroppingSpeed.GetComponent<FuelRespawn>().instanceSpeed - 0.1f;
+
         }
     }
+
+
+    IEnumerator backgroundTransition()
+    {
+        while(distance != 9)
+        {
+            yield return new WaitForSeconds(30f);
+            if (distance == 1)
+            {
+                StartCoroutine("fadeOutSprite");
+                StartCoroutine("fadeInSprite"); 
+            }
+            else if (distance == 3)
+            {
+                StartCoroutine("fadeOutSprite");
+                StartCoroutine("fadeInSprite");          
+            }
+            else if (distance == 4)
+            {
+                StartCoroutine("fadeOutSprite");
+                StartCoroutine("fadeInSprite");
+            }
+            else if (distance == 6)
+            {
+                StartCoroutine("fadeOutSprite");
+                StartCoroutine("fadeInSprite");              
+            }
+            else if (distance == 8)
+            {
+                StartCoroutine("fadeOutSprite");
+                StartCoroutine("fadeInSprite");            
+            }
+            else if (distance == 9)
+            {
+                StartCoroutine("fadeOutSprite");
+                StartCoroutine("fadeInSprite");            
+            }
+        }
+    }
+
+    IEnumerator fadeInSprite()
+    {
+        for (float f = 0.1f; f <= 1; f +=0.1f)
+        {
+            Color c = backgroundRenderer.color;
+            c.a = f;
+            backgroundRenderer.color = c;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator fadeOutSprite()
+    {
+        for (float f = 1f; f >= 0f; f -= 0.1f)
+        {
+            Color c = backgroundRenderer.color;
+            c.a = f;
+            backgroundRenderer.color = c;
+            yield return new WaitForSeconds(2.5f);
+        }
+    }
+
+
 }
